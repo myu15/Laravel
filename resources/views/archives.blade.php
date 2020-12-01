26112900b8573fa672c02571267e2a0994e78b3b
yuka <yk15.e.oras2@icloud.com>
2020-12-02 02:20:37 +0900

    first commit

---
+++ resources/views/archives.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>記事一覧</title>
+  </head>
+  <body>
+    <h1>記事一覧</h1>
+
+    <p>viewで生成しました</p>
+  </body>
+</html>
