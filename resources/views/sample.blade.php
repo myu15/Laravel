a80e53c8564bc5bc2ef5ec631703b3898b2cfa94
yuka <yk15.e.oras2@icloud.com>
2020-12-02 02:32:29 +0900

    HTML編集

--- resources/views/sample.blade.php
+++ resources/views/sample.blade.php
@@ -1,17 +1,10 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-  <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Title</title>
-</head>
-<body>
-<h1>sample</h1>
-<div id="app">
-    <example-component></example-component>
-</div>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
 
-<script src="{{ asset('/js/app.js') }}"></script>
-</body>
+  </body>
 </html>
